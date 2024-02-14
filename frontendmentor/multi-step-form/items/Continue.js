b95e64d45aa60798301d08c1b08c0ee0acdecfd1
Kauavillan <61887808+Kauavillan@users.noreply.github.com>
2024-02-13 21:05:56 -0300

    Delete frontendmentor/multi-step-form directory

--- frontendmentor/multi-step-form/items/Continue.js
+++
@@ -1,8 +0,0 @@
-import styles from "../styles/Continue.module.css";
-export default function Continue({text="Next Step", action}){
-    return(
-        <button onClick={action} className={`${text == "Confirm" && styles.confirm} ${styles.continue}`}>
-            {text}
-        </button>
-    )
-}
--- frontendmentor/multi-step-form/items/Return.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Return.module.css";
-
-export default function Return({action}){
-    return(
-        <button onClick={action} className={styles.return}>
-            Go back
-        </button>
-    )
-}
